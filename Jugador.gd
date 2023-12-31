652cf6e60e404ae00aa79bc2e922e686d60af4a3
Manu Zarraga <manuzarraga@hotmail.com>
2023-12-31 13:38:26 -0300

    feat: adding files

---
+++ Jugador.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Animaciones.play("Piola")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = direction * 200
+	move_and_slide()
